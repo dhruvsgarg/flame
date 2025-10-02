e44fa09c2dc39310e1043061b9069ea2957c6b7b
Neha Lakhani <neha.lakhani1805@gmail.com>
2025-10-02 17:57:30 -0400

    Basic setup changes

--- lib/python/flame/mode/horizontal/syncfl/fwdllm_aggregator.py
+++ lib/python/flame/mode/horizontal/syncfl/fwdllm_aggregator.py
@@ -118,7 +118,7 @@ class TopAggregator(SyncTopAgg):
         # maintain a set of all trainers that have sent heartbeats previously
         self.all_trainers = set()
         try:
-            self.minInitialTrainers = self.config.selector.kwargs.get("minInitialTrainers")
+            self.minInitialTrainers = self.config.selector.kwargs.get("minTrainersToStart")
             assert self.minInitialTrainers is not None
         except (KeyError, AssertionError):
             raise KeyError("minInitialTrainers must be specified in selector config & must not be None for determinism")
